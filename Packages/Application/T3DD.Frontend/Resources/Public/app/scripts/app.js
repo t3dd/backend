dc8aebf66e2286789221b68a7985b3c1de37cd34
Thomas Maroschik <tmaroschik@dfau.de>
2015-03-03 20:48:48 +0100

    [TASK] Footer & Back to Top Link

--- Packages/Application/T3DD.Frontend/Resources/Public/app/scripts/app.js
+++ Packages/Application/T3DD.Frontend/Resources/Public/app/scripts/app.js
@@ -1,9 +1,16 @@
 (function(document) {
 	'use strict';
 
-	//function() {
-	//
-	//};
+	function smoothStep(start, end, point) {
+		if(point <= start) {
+			return 0
+		}
+		if(point >= end) {
+			return 1
+		}
+		var x = (point - start) / (end - start);
+		return x * x * (3 - 2 * x)
+	}
 
 	var template = document.getElementById('t');
 
@@ -19,6 +26,28 @@
 	template.logout = function() {
 		document.getElementById('baseLogin').logout();
 		document.getElementById('userLogin').opened = false;
+	};
+
+	template.scrollToTop = function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		var scrollContainer = document.getElementById('scrollHeader').shadowRoot.getElementById('mainContainer');
+		var startTime = performance.now();
+		var endTime = startTime + 250;
+		var startTop = scrollContainer.scrollTop;
+		var destY = startTop * -1;
+		if(destY === 0) {
+			return
+		}
+		var callback = function(timestamp) {
+			if(timestamp < endTime) {
+				requestAnimationFrame(callback)
+			}
+			var point = smoothStep(startTime, endTime, timestamp);
+			var scrollTop = Math.round(startTop + destY * point);
+			scrollContainer.scrollTop = scrollTop;
+		};
+		callback(startTime)
 	};
 
 	template.addEventListener('template-bound', function() {
