1e1e9850ab1e1e78a11930a9b38fead1aa7d8ad4
Thomas Maroschik <tmaroschik@dfau.de>
2015-03-05 16:08:08 +0100

    [FEATURE] Login Interceptor
     /  _  \
    |-=(_)=-|
     \     /

--- Packages/Application/T3DD.Frontend/Resources/Public/app/scripts/app.js
+++ Packages/Application/T3DD.Frontend/Resources/Public/app/scripts/app.js
@@ -12,20 +12,45 @@
 		return x * x * (3 - 2 * x)
 	}
 
-	var template = document.getElementById('t');
+	var template = document.getElementById('t'),
+		firstRequest = true;
 
 	template.register = function() {
-		document.querySelector('app-router').go('/register');
+		this.$.router.go('/register');
 	};
 
 	template.login = function() {
-		document.getElementById('baseLogin').login();
-		document.getElementById('userLogin').opened = false;
+		this.$.baseLogin.login();
+		this.$.userLogin.opened = false;
 	};
 
 	template.logout = function() {
-		document.getElementById('baseLogin').logout();
-		document.getElementById('userLogin').opened = false;
+		this.$.baseLogin.logout();
+		this.$.userLogin.opened = false;
+	};
+
+	template.requireLogin = function(pathOrCallback) {
+		var listener = (function() {
+			if (this.currentPath !== pathOrCallback) {
+				this.$.router.go(this.currentPath);
+			} else {
+				this.$.router.go('/');
+			}
+			this.$.loginInterceptor.removeEventListener('core-overlay-close-completed', listener);
+		}).bind(this);
+
+		this.$.loginInterceptor.opened = true;
+		this.$.loginInterceptor.addEventListener('core-overlay-close-completed', listener);
+
+		this.observeOnce('globals.user', function() {
+			this.$.loginInterceptor.removeEventListener('core-overlay-close-completed', listener);
+			this.$.loginInterceptor.opened = false;
+			if (typeof pathOrCallback === 'string') {
+				this.$.router.go(pathOrCallback);
+			} else {
+				pathOrCallback();
+			}
+		});
 	};
 
 	template.scrollToTop = function(event) {
@@ -52,14 +77,45 @@
 
 	template.addEventListener('template-bound', function() {
 		this.globals.scrollTarget = this.$.scrollHeader.shadowRoot.getElementById('mainContainer');
-		this.$.router.addEventListener('activate-route-end', function(event) {
+		this.$.router.addEventListener('activate-route-end', (function(event) {
+			if (firstRequest) {
+				this.interceptRouting(event);
+			}
 			template.globals.currentPath = template.currentPath = event.detail.path;
-		});
+			firstRequest = false;
+		}).bind(this));
+		this.$.router.addEventListener('activate-route-start', this.interceptRouting.bind(this));
 	});
 
 	template.changePage = function(event) {
 		if (!event.detail.isSelected) return;
 		this.$.router.go(this.currentPath);
+	};
+
+	template.isLoginRequired = function(path) {
+		var forceLoginRoute = this.$.router.querySelector('[path="' + path + '"][forceLogin]');
+		return !!forceLoginRoute;
+	}
+
+	template.interceptRouting = function(event) {
+		var path = event.detail.path;
+		if (!this.isLoginRequired(path)) return;
+		event.preventDefault();
+		if (!this.statusKnown) {
+			this.observeOnce('statusKnown', function(newValue) {
+				if (newValue) this.async(function() { this.interceptRouting(event); }.bind(this));
+			});
+			return;
+		}
+		this.requireLogin(path);
+	};
+
+	template.observeOnce = function(attributeName, listener) {
+		var observer = new PathObserver(this, attributeName);
+		observer.open(function(newValue, oldValue) {
+			listener.call(this, newValue, oldValue);
+			observer.close();
+		}.bind(this));
 	};
 
 })(wrap(document));
