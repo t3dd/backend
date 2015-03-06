fe0eeff453bed87dec03da898d9ea3958032812f
Sascha Nowak <sascha.nowak@netlogix.de>
2015-03-06 02:55:52 +0100

    [TASK] Context switch

--- Packages/Application/T3DD.Frontend/Classes/T3DD/Frontend/Controller/FrontendController.php
+++ Packages/Application/T3DD.Frontend/Classes/T3DD/Frontend/Controller/FrontendController.php
@@ -1,15 +1,45 @@
 <?php
 namespace T3DD\Frontend\Controller;
 
+/*                                                                        *
+ * This script belongs to the TYPO3 T3DD Frontend.                        *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
 /**
  * Enter descriptions here
  */
 class FrontendController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 
 	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Utility\Environment
+	 */
+	protected $environment;
+
+	/**
+	 * @var string
+	 */
+	protected $targetDirectory = 'dist';
+
+	public function initializeServeAction() {
+		if ($this->environment->getContext()->isDevelopment()) {
+			$this->targetDirectory = 'app';
+		}
+	}
+
+	/**
 	 */
 	public function serveAction() {
-		$html = file_get_contents('resource://T3DD.Frontend/Public/dist/index.html');
+
+		$html = file_get_contents('resource://T3DD.Frontend/Public/' . $this->targetDirectory . '/index.html');
 		return $this->rewriteResourceUrls($html);
 	}
 
@@ -19,7 +49,7 @@ class FrontendController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	 */
 	protected function rewriteResourceUrls($html) {
 		return preg_replace_callback('!(?:<(?:link|script|img|app-route)\s.*?>)+!i', function($matches) {
-			return preg_replace('!(href|import|src)="([^/\{].*?)"!i', '${1}="/_Resources/Static/Packages/T3DD.Frontend/dist/${2}"', $matches[0]);
+			return preg_replace('!(href|import|src)="([^/\{].*?)"!i', '${1}="/_Resources/Static/Packages/T3DD.Frontend/' . $this->targetDirectory . '/${2}"', $matches[0]);
 		}, $html);
 	}
 
